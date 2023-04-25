a4dbd6be05cdbbd3ec4e56d5aa7efe56d6b1ba5e
Ahmed Algzery <algzeryahmed14@gmail.com>
2023-04-25 16:59:55 +0200

    add get package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
   runApp(const BookStorApp());
@@ -8,6 +9,8 @@ class BookStorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GetMaterialApp(
+      home: SplashView(),
+    );
   }
 }
