9adda819129937559f87f4adab3fef1e96c6eee3
Ahmed Algzery <algzeryahmed14@gmail.com>
2023-04-25 17:04:15 +0200

    add Readme

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() {
-  runApp(const BookStorApp());
+  runApp(const BooksStore());
 }
-class BookStorApp extends StatelessWidget {
-  const BookStorApp({super.key});
+
+class BooksStore extends StatelessWidget {
+  const BooksStore({super.key});
 
   @override
   Widget build(BuildContext context) {
