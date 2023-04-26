bf78c3c11d4805cc6d8c86c005434b2e0122fca9
Ahmed Algzery <algzeryahmed14@gmail.com>
2023-04-26 17:37:02 +0200

    create styles file & add text titel to home view

--- lib/features/Home/presentation/view/widgets/home_view_body.dart
+++ lib/features/Home/presentation/view/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookstore/core/utils/styles.dart';
 import 'package:bookstore/features/Home/presentation/view/widgets/custom_app_bar.dart';
 import 'package:bookstore/features/Home/presentation/view/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -7,11 +8,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.only(left: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50.0,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
