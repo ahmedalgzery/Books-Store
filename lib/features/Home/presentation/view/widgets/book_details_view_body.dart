d68232257d48ce00a21a4bbaf3d18cbe9fd2f30f
Ahmed Algzery <algzeryahmed14@gmail.com>
2023-04-28 18:03:05 +0300

    add image to book details view

--- lib/features/Home/presentation/view/widgets/book_details_view_body.dart
+++ lib/features/Home/presentation/view/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookstore/features/Home/presentation/view/widgets/custom_book_details_app_bar.dart';
+import 'package:bookstore/features/Home/presentation/view/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,11 +7,16 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30.0),
+    double width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         children: [
-          CustomBookDetailesAppBar(),
+          const CustomBookDetailesAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: const CustomListViewItem(),
+          ),
         ],
       ),
     );
